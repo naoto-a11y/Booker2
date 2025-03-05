a0b021298801d1e3f1f48f8640f4cd4dce3c1338
EC2 Default User <ec2-user@ip-172-31-6-245.ap-northeast-1.compute.internal>
2025-03-05 02:20:48 +0000

    削除機能追加 詳細リンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
